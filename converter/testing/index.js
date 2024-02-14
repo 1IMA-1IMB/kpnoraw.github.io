5ab2e105d0c986aa14d95d450d1116db67b29e57
NWilhelmine <nora.grendal@gmail.com>
2024-02-14 11:20:47 +0100

    converter oppgave

--- converter/testing/index.js
+++
@@ -1,6 +0,0 @@
-function measureFunc(){
-    const uInput = document.getElementById("fInput").value
-    const from = parseFloat(document.getElementById("from").value)
-    const to = parseFloat(document.getElementById("to").value)
-    document.getElementById("fOutput").innerHTML = (uInput * from)/ to
-}
